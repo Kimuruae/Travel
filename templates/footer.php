737870fa8b2fbe082d51f9ca9b31237bb5157c1a
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-07-02 22:04:37 +0300

    php template for header,footer, navigation bar and side bar

---
+++ templates/footer.php
@@ -0,0 +1,18 @@
+              <!--Footer-->
+              <footer class="footer mt-4 bg-dark text-white text-center py-3">
+                <div class="container">
+                  <p>&copy; 2024 Tour Kenya. All Rights Reserved.</p>
+                  <ul class="list-inline">
+                    <li class="list-inline-item"><a href="#" class="text-white">Terms and Conditions</a></li>
+                    <li class="list-inline-item"><a href="#" class="text-white">Privacy Policy</a></li>
+                    <li class="list-inline-item"><a href="#" class="text-white">Cookies</a></li>
+                    <li class="list-inline-item"><a href="#" class="text-white">Complaints</a></li>
+                  </ul>
+                </div>
+              </footer>
+    <!--Bootrap Js,Pooper,jQuery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tour Kenya</title>
+    <!--Bootsrap CSS for a responsive Webpage-->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!--Custom CSS-->
+    <link href="..//css/style.css" rel="stylesheet">
+
+</head>
+<body>
