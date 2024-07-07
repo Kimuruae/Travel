c5c8d7709a62059d654d47bd69d3d40e53bea9be
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-07-07 13:26:01 +0300

    New features added to the side bar

---
+++ templates/side_bar.php
@@ -0,0 +1,29 @@
+      <!-- Sidebar Section -->
+      <div class="col-md-4">
+        <!-- Categories -->
+        <div class="card mb-4">
+          <div class="card-header">
+            Categories
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="#">Wildlife</a></li>
+            <li class="list-group-item"><a href="#">Beach Holidays</a></li>
+            <li class="list-group-item"><a href="#">Cultural Tours</a></li>
+            <li class="list-group-item"><a href="#">Adventure Activities</a></li>
+            <li class="list-group-item"><a href="#">City Excursions</a></li>
+          </ul>
+        </div>
+        <!-- Recent Posts -->
+        <div class="card mb-4">
+          <div class="card-header">
+            Recent Posts
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item"><a href="post1.html">Exploring the Wildlife of Kenya</a></li>
+            <li class="list-group-item"><a href="post2.html">Top Beach Destinations in Kenya</a></li>
+            <li class="list-group-item"><a href="post3.html">Cultural Experiences in Kenya</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div
