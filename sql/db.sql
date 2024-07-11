de363448d0b927cf14e2010446560c45d80bca4e
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-07-11 14:18:24 +0300

    Deleted a table

--- sql/db.sql
+++ sql/db.sql
@@ -46,20 +46,4 @@ CREATE TABLE IF NOT EXISTS `messages` (
 TRUNCATE TABLE `messages`;
 
 -- Dumping data for table `messages`
--- Table structure for table Bookings
 
-DROP TABLE IF EXISTS `bookings`;
-CREATE TABLE IF NOT EXISTS `bookings` (
-  `userId` bigint(11) NOT NULL AUTO_INCREMENT,
-  `fullname` varchar(50) NOT NULL DEFAULT '',
-  `email` varchar(50) NOT NULL DEFAULT '',
-  `username` varchar(50) NOT NULL DEFAULT '',
-  `password` varchar(60) NOT NULL DEFAULT '',
-  `datecreated` datetime NOT NULL DEFAULT current_timestamp(),
-  `dateupdated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`userId`),
-  UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-TRUNCATE TABLE `bookings`;
