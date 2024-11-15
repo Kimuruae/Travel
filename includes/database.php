6215bcf7d4f8f0a1645dbebd1ed5a5781a4c4a2c
Edwin Kimuruae <sururu.kimuruae@kimuruae.edu>
2024-11-15 13:52:05 +0300

    New update

--- includes/database.php
+++ includes/database.php
@@ -3,7 +3,7 @@ session_start();
 $db_server = "localhost";
 $db_username = "root";
 $db_pass = "";
-$db_name = "wad";
+$db_name = "Travel";
 
 // Create connection
 $conn = new mysqli($db_server, $db_username, $db_pass,$db_name);
